26fafd63b18b37c6d4f6494d6fd53e5af2fd1752
sofiamorenosamsung <sofiamorenosamsung@gamail.com>
2023-07-31 21:09:45 -0300

    Agregué pruebas con otros valores ej a

--- js/ejercicio-a.js
+++ js/ejercicio-a.js
@@ -1,4 +1,51 @@
-var cantDeGatos=10
+console.log('Ejercicio 06-a');
+console.log('Var cantDeGatos =10');
+
+ var cantDeGatos=10
+for ( var gato = 1 ; gato<= cantDeGatos; gato++){
+var contador = gato% 3 ;
+switch(contador){
+    case 0: 
+     console.log ('Gato '+'#'+ gato +'😺')
+    break;
+    
+    case 1:
+       console.log ('Gato '+'#'+ gato +'😸')
+        break;
+
+    case 2 : 
+      console.log('Gato '+'#'+ gato +'😹')
+        break;
+}
+} 
+
+//Prueba con otra cantidad de var cantDeGatos
+
+console.log('Prueba con un valor distinto para cantDeGatos = 6');
+
+var cantDeGatos=6
+for ( var gato = 1 ; gato<= cantDeGatos; gato++){
+var contador = gato% 3 ;
+switch(contador){
+    case 0: 
+     console.log ('Gato '+'#'+ gato +'😺')
+    break;
+    
+    case 1:
+       console.log ('Gato '+'#'+ gato +'😸')
+        break;
+
+    case 2 : 
+      console.log('Gato '+'#'+ gato +'😹')
+        break;
+}
+}
+
+//Prueba con otra cantidad de var cantDeGatos
+
+console.log('Otra prueba con otro valor var cantDeGatos= 5');
+
+var cantDeGatos=7
 for ( var gato = 1 ; gato<= cantDeGatos; gato++){
 var contador = gato% 3 ;
 switch(contador){
